8406dc767fd6fd06f7ac665abead881899974a52
WenboMeng <38266142+wenbomeng95@users.noreply.github.com>
2019-05-04 22:53:11 -0400

    Add files via upload

---
+++ var_set.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+
+deviceType = "pi"
+
+path = '/home/pi/live'
+
+
+
+
+rtmp = 'rtmp://txy.live-send.acg.tv/live-txy/'
+
+live_code = '?streamname=live_3801460_5165623&key=6722d8282c6d4d90e6e74dbd970b1d69'
+
+

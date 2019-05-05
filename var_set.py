d4816f600d24cf86e21e18addb820cecfd75ff32
WenboMeng <38266142+wenbomeng95@users.noreply.github.com>
2019-05-04 23:27:18 -0400

    Add files via upload

--- var_set.py
+++ var_set.py
@@ -1,15 +1,1 @@
-#coding:utf-8
-
-
-deviceType = "pi"
-
-path = '/home/pi/live'
-
-
-
-
-rtmp = 'rtmp://txy.live-send.acg.tv/live-txy/'
-
-live_code = '?streamname=live_3801460_5165623&key=6722d8282c6d4d90e6e74dbd970b1d69'
-
-
+#coding:utf-8deviceType = "pi"path = '/home/pi/live'roomid = '783006'cookie = ''rtmp = 'rtmp://txy.live-send.acg.tv/live-txy/'live_code = '?streamname=live_3801460_5165623&key=6722d8282c6d4d90e6e74dbd970b1d69'
